e7ce94cb89823bf195c4835ffda7b4f04a6b3535
Cherrypick14 <cherrypicklet@gmail.com>
2023-09-27 01:19:52 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+      
+        <p className='text-3xl font-bold underline text-red-700'>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+    
+    </>
+  )
+}
+
+export default App

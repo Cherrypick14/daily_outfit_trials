199f49c1dbe728f050e37d86f17f69c01d153086
Cherrypick14 <cherrypicklet@gmail.com>
2023-09-28 00:36:44 +0300

    add grid layout to our outfit closet

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Outfits from './components/outfits'
 
 
 function App() {
+  
   const daysofweek =[
     'Monday',
     'Tuesday', 
@@ -15,14 +16,15 @@ function App() {
     'Sunday']
   return (
     <>
-      <div className="card">
-        <h1 className='text-red-600 text-lg'>Welcome t my Outfit Closet </h1>
+      <div className="container mx-auto mt-8 p-4">
+        <h1 className='text-3xl font-semibold mb-4'>Welcome to my Outfit Closet </h1>
+         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
 
-      {daysofweek.map((day)=>{
+         {daysofweek.map((day)=>{
 
-         <Outfits key={day} day={day} />
-      })}
-     
+              <Outfits key={day} day={day} />
+           })}
+         </div>
       </div>
     
     </>

56cd04a4241e8f91cbe0185f1161c4300761ba52
Cherrypick14 <cherrypicklet@gmail.com>
2023-09-28 00:28:55 +0300

    redo outfit week selection

---
+++ outfitdata.js
@@ -0,0 +1,40 @@
+const outfits = {
+
+    "Monday": [
+        'Casual jeans and T-shirt',
+        'Blue dress', 
+        'Formal suit'
+      ],
+    "Tuesday": [
+        'Red skirt and white top',
+        'Black jeans and sweater', 
+        'Business casual suit'
+    ],
+    "Wednesday": [
+        'Green dress', 
+        'Shorts and a tank top', 
+        'Workout clothes'
+    ],
+    "Thursday": [
+        'Brown trousers and blouse',
+        'Sporty look with sneakers', 
+        'Semi-formal suit'
+    ],
+    "Friday": [
+        'Party dress',
+        'Casual Friday denim look', 
+        'Night out sleek dress'
+    ],
+    "Saturday": [
+        'Beach attire',
+        'Laid back shorts and tee',
+        'Date night dress'
+    ],
+    "Sunday": [
+        'Comfy PJs',
+        'Sunday brunch dress', 
+        'Relaxed fit for a day in'
+    ],
+}
+
+export default outfits;
